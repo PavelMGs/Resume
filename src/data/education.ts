5dd3a327d3edb4e3d431b90db82fec01b3508ba8
PavelMGs <pavelmg99@gmail.com>
2021-05-17 12:15:40 +0300

    feat: remove all pdf

--- src/data/education.ts
+++ src/data/education.ts
@@ -1,13 +1,13 @@
-import JSMarathon from '../assets/JSMarathon.pdf';
-import ReactMarathon from '../assets/ReactMarathon.pdf';
-import RSCertificate from '../assets/RSCertificate.pdf';
+// import JSMarathon from '../assets/JSMarathon.pdf';
+// import ReactMarathon from '../assets/ReactMarathon.pdf';
+// import RSCertificate from '../assets/RSCertificate.pdf';
 
 export const additionalEducation = [
     {
         name: 'JS-marathon',
         description: 'A three-week marathon in native JS with a simple card game as a project. Zar Zakharov taught the material and conducted the code review.',
         organization: 'Zar Zakharov',
-        certificate: JSMarathon,
+        // certificate: JSMarathon,
         projects: [{
             name: 'Pokemon Game',
             repository: 'https://github.com/PavelMGs/js-marathon',
@@ -17,7 +17,7 @@ export const additionalEducation = [
         name: 'React-marathon',
         description: 'The next marathon after JS-marathon. The project is a simple app for learning English words. My first experience with React, for this reason, the application works clumsily, in the near future it will be improved.',
         organization: 'Zar Zakharov',
-        certificate: ReactMarathon,
+        // certificate: ReactMarathon,
         notdeployed: true
         // projects: [{
         //     name: 'Learn The Words',
@@ -38,7 +38,7 @@ export const additionalEducation = [
         name: 'ReactCourse',
         description: 'Two-month course from RollingScopes School. There were three projects within the course, two of them were team projects. Links to projects below.',
         organization: 'RSSchool',
-        certificate: RSCertificate,
+        // certificate: RSCertificate,
         projects: [
             {
                 name: 'Snake',
